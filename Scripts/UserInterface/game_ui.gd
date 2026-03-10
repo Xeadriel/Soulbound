2b28abbacdf3fbaece3f9098eb9c014445bbd494
Mono-Kaze <williamtanjaya@gmail.com>
2026-03-10 17:44:40 +0100

    pause menu UI built into main scene with camera offset

--- Scripts/UserInterface/game_ui.gd
+++ Scripts/UserInterface/game_ui.gd
@@ -1,13 +1,13 @@
 extends Control
 
 var pages = []
-@export var current_page = 0
 var currentPageIndex = 0;
 var pageWidth;
+var cameraOffset; 
 
 func _ready() -> void:
-	pageWidth = $UI1.size.x;
-	
+	pageWidth = $UI1.size.x ;
+	cameraOffset = pageWidth / 2;
 	pages = [
 		$UI1,
 		$UI2,
@@ -18,16 +18,23 @@ func _ready() -> void:
 		pages[i].position.x = i * pageWidth;
 	
 func _unhandled_input(event: InputEvent) -> void:
-	if EventHandler.isPlayerInputJustPressed("right"):
+	if EventHandler.isPlayerInputJustPressed("pause"):
+		if(self.visible):
+			self.visible = false;
+			currentPageIndex = 0;
+			self.position.x = -cameraOffset;
+		else:
+			self.visible = true;
+	elif self.visible && EventHandler.isPlayerInputJustPressed("right"):
 		if(currentPageIndex < pages.size() - 1):
 			currentPageIndex += 1;
 			var tw = create_tween();
-			tw.tween_property(self, "position:x", -currentPageIndex * pageWidth, 0.3);
-	elif EventHandler.isPlayerInputJustPressed("left"):
+			tw.tween_property(self, "position:x", -currentPageIndex * pageWidth - cameraOffset, 0.3);
+	elif self.visible && EventHandler.isPlayerInputJustPressed("left"):
 		if(currentPageIndex > 0):
 			currentPageIndex -= 1;
 			var tw = create_tween();
-			tw.tween_property(self, "position:x", -currentPageIndex * pageWidth, 0.3);
+			tw.tween_property(self, "position:x", -currentPageIndex * pageWidth - cameraOffset, 0.3);
 
 func _process(delta: float) -> void:
 	pass
