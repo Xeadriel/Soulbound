e5fa5fe61664fe51b697a42c33addb2f15e303de
Mono-Kaze <williamtanjaya@gmail.com>
2026-03-12 18:28:55 +0100

    button combinations for adding items into a quickslot

--- Scripts/UserInterface/Inventory.gd
+++ Scripts/UserInterface/Inventory.gd
@@ -3,8 +3,7 @@ extends Control
 @export var columns = 7
 @export var rows = 3
 
-#player number starts from 0
-var selectedPlayerIndex = [0, 0]
+var playerSelectedIndex = [0, 0]
 
 var itemEnumsList = MenuGlobals.ItemIndices
 
@@ -14,67 +13,88 @@ var itemScenes = {
 
 func _ready() -> void:
 	#just as a test
-	addItemBuilder(itemEnumsList.POTION)
-	addItemBuilder(itemEnumsList.POTION)
+	addItemDispatcher(itemEnumsList.POTION)
+	addItemDispatcher(itemEnumsList.POTION)
 	
 	updateSelector()
 	
 
 func _process(delta: float) -> void:
 	if self.has_focus():
-		if EventHandler.isPlayerInputJustPressed("right"):
-			moveSelector(1, 0, 0)
-		elif EventHandler.isPlayerInputJustPressed("left"):
-			moveSelector(-1, 0, 0)
-		elif EventHandler.isPlayerInputJustPressed("up"):
-			moveSelector(0, -1, 0)
-		elif EventHandler.isPlayerInputJustPressed("down"):
-			moveSelector(0, 1, 0)
-		elif EventHandler.isPlayerInputJustPressed("left2"):
-			moveSelector(-1, 0, 1)
-		elif EventHandler.isPlayerInputJustPressed("up2"):
-			moveSelector(0, -1, 1)
-		elif EventHandler.isPlayerInputJustPressed("down2"):
-			moveSelector(0, 1, 1)
-		elif EventHandler.isPlayerInputJustPressed("right2"):
-			moveSelector(1, 0, 1)
-	
+		
+		if EventHandler.isPlayerInputJustPressed("right") && !EventHandler.isPlayerInputPressed("hit"):
+				moveSelector(1, 0, 0)
+		elif EventHandler.isPlayerInputJustPressed("left") && !EventHandler.isPlayerInputPressed("hit"):
+				moveSelector(-1, 0, 0)
+		elif EventHandler.isPlayerInputJustPressed("up") && !EventHandler.isPlayerInputPressed("hit"):
+				moveSelector(0, -1, 0)
+		elif EventHandler.isPlayerInputJustPressed("down") && !EventHandler.isPlayerInputPressed("hit"):
+				moveSelector(0, 1, 0)
+		elif EventHandler.isPlayerInputJustPressed("left2") && !EventHandler.isPlayerInputPressed("hit2"):
+				moveSelector(-1, 0, 1)
+		elif EventHandler.isPlayerInputJustPressed("up2") && !EventHandler.isPlayerInputPressed("hit2"):
+				moveSelector(0, -1, 1)
+		elif EventHandler.isPlayerInputJustPressed("down2") && !EventHandler.isPlayerInputPressed("hit2"):
+				moveSelector(0, 1, 1)
+		elif EventHandler.isPlayerInputJustPressed("right2") && !EventHandler.isPlayerInputPressed("hit2"):
+				moveSelector(1, 0, 1)
+		elif EventHandler.isPlayerInputPressed("hit") && EventHandler.isPlayerInputPressed("right"):
+			toQuickSlot(playerSelectedIndex[0], "right")
+		elif EventHandler.isPlayerInputPressed("hit") && EventHandler.isPlayerInputPressed("left"):
+			toQuickSlot(playerSelectedIndex[0], "left")
+		elif EventHandler.isPlayerInputPressed("hit") && EventHandler.isPlayerInputPressed("up"):
+			toQuickSlot(playerSelectedIndex[0], "up")
+		elif EventHandler.isPlayerInputPressed("hit") && EventHandler.isPlayerInputPressed("down"):
+			toQuickSlot(playerSelectedIndex[0], "down")
+		elif EventHandler.isPlayerInputPressed("hit2") && EventHandler.isPlayerInputPressed("right2"):
+			toQuickSlot(playerSelectedIndex[1], "right2")
+		elif EventHandler.isPlayerInputPressed("hit2") && EventHandler.isPlayerInputPressed("left2"):
+			toQuickSlot(playerSelectedIndex[1], "left2")
+		elif EventHandler.isPlayerInputPressed("hit2") && EventHandler.isPlayerInputPressed("up2"):
+			toQuickSlot(playerSelectedIndex[1], "up2")
+		elif EventHandler.isPlayerInputPressed("hit2") && EventHandler.isPlayerInputPressed("down2"):
+			toQuickSlot(playerSelectedIndex[1], "down2")
+
+func toQuickSlot(itemIndex, quickslot):
+	print("to quick Slot : " + str(itemIndex) + quickslot)
 
 func moveSelector(dx: int, dy: int, playerNumber: int) -> void:
-	var row = selectedPlayerIndex[playerNumber] / columns
-	var col = selectedPlayerIndex[playerNumber] % columns
+	var row = playerSelectedIndex[playerNumber] / columns
+	var col = playerSelectedIndex[playerNumber] % columns
 	col += dx
 	row += dy
 	row = clamp(row, 0, rows - 1)
 	col = clamp(col, 0, columns - 1)
-	selectedPlayerIndex[playerNumber] = row * columns + col
+	playerSelectedIndex[playerNumber] = row * columns + col
 	updateSelector()
 
 func updateSelector():
 	var gc = $MarginContainer/VBoxContainer/InventoryBox/ItemList2/GridContainer
 	for i in gc.get_child_count():
 		var slot = gc.get_child(i)
-		if selectedPlayerIndex[0] == i:
+		if playerSelectedIndex[0] == i:
 			slot.modulate = Color(0, 0, 1)
-		elif selectedPlayerIndex[1] == i:
+		elif playerSelectedIndex[1] == i:
 			slot.modulate = Color(1, 0, 0)
-		elif selectedPlayerIndex[0] != i && selectedPlayerIndex[1] != i:
+		elif playerSelectedIndex[0] != i && playerSelectedIndex[1] != i:
 			slot.modulate = Color(0.3, 0.3, 0.3)
 
-func addItemBuilder(itemEnum: int) -> void:
-	if itemEnum in itemEnumsList.values():
-		var itemName
-		var itemScene
-		match itemEnum:
-			itemEnumsList.POTION:
-				itemName = "Potion"
-				itemScene = itemScenes[itemEnum]
-				
-			#add more items here
+func addItemDispatcher(itemEnum: int) -> void:
+	var itemName = null
+	var itemScene = null
+	match itemEnum:
+		itemEnumsList.POTION:
+			itemName = "Potion"
+			itemScene = itemScenes[itemEnum]
 			
+		#
+		#add more items here
+		#
+		
+	if itemName != null && itemScene != null:
 		addItem(itemName, itemScene)
 	else:
-		push_error("Invalid Item Enum value!")
+		print("Invalid Item Enum value!")
 
 func addItem(itemName: String, itemScene):
 	var itemSlots = $MarginContainer/VBoxContainer/InventoryBox/ItemList2/GridContainer.get_children()
