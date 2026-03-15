00923fe0f52a7740751d55b42712b1e6ffe41b81
Xeadriel <ogulcanasik@web.de>
2026-03-15 18:04:11 +0100

    lever example for properties

--- Scripts/World/WorldObjects/Lever.gd
+++ Scripts/World/WorldObjects/Lever.gd
@@ -1,6 +1,11 @@
 class_name Lever extends WorldObject
 
+var leverState = false
+
 # check which traits (destructible, interactible, obstacle etc.) are assigned
 func _ready() -> void:
 	pass 
+
+func leverPulled() -> void:
+	leverState = not leverState
 
