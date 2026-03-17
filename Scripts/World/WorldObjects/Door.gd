7b0a94708e75d959d6d625ff57d8930bae3dddf4
Mono-Kaze <williamtanjaya@gmail.com>
2026-03-17 13:24:29 +0100

    removed label for interactable -> outline preferable

--- Scripts/World/WorldObjects/Door.gd
+++ Scripts/World/WorldObjects/Door.gd
@@ -6,19 +6,7 @@ func _ready() -> void:
 	$PropertyInteractable.interacted.connect(interactDoor)
 
 func _process(delta: float) -> void:
-	var p1isClose = $PropertyInteractable.player1CloseEnough
-	var p2isClose = $PropertyInteractable.player2CloseEnough
-	if p1isClose && !p2isClose:
-		$Label.visible = true
-		$Label.text = "press e"
-	elif !p1isClose && p2isClose:
-		$Label.visible = true
-		$Label.text = "press o"
-	elif p1isClose && p2isClose:
-		$Label.visible = true
-		$Label.text = "press e or o"
-	else:
-		$Label.visible = false
+	pass
 
 func interactDoor():
 	if isOpen:
