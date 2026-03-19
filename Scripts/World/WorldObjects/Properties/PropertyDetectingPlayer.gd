920b4b47bfaa177a622a2b19c9eae4d3ac4d6eb5
Mono-Kaze <williamtanjaya@gmail.com>
2026-03-19 11:33:11 +0100

    property detecting player emits its own signal

--- Scripts/World/WorldObjects/Properties/PropertyDetectingPlayer.gd
+++ Scripts/World/WorldObjects/Properties/PropertyDetectingPlayer.gd
@@ -3,15 +3,19 @@ extends Area2D
 var isPlayer1Inside = false
 var isPlayer2Inside2 = false
 
+signal detected
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		body = body as Player
 		if body is Player1:
 			isPlayer1Inside = true
 			EventHandler.player1Entered.emit(self)
+			detected.emit()
 		elif body is Player2:
 			isPlayer2Inside2 = true
 			EventHandler.player2Entered.emit(self)
+			detected.emit()
 	if isPlayer1Inside && isPlayer2Inside2:
 		EventHandler.bothPlayersEntered.emit(self)
 
@@ -21,8 +25,10 @@ func _on_body_exited(body: Node2D) -> void:
 		if body is Player1:
 			isPlayer1Inside = false
 			EventHandler.player1Exited.emit(self)
+			detected.emit()
 		elif body is Player2:
 			isPlayer2Inside2 = false
 			EventHandler.player2Exited.emit(self)
+			detected.emit()
 	if !isPlayer1Inside && !isPlayer2Inside2:
 		EventHandler.bothPlayersExited.emit(self)
