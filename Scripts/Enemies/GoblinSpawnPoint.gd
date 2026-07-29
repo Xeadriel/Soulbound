8e05768a8e84d63a30238ee4c3d933d92e5bcfe1
Mono-Kaze <williamtanjaya@gmail.com>
2026-07-29 18:44:04 +0200

    enemy spawner implemented

--- Scripts/Enemies/GoblinSpawnPoint.gd
+++ Scripts/Enemies/GoblinSpawnPoint.gd
@@ -1,21 +1,48 @@
 extends Node2D
 
-@export var enemyScenes := {
-	"goblin": preload("res://Scenes/Enemies/Goblin.tscn"),
-	"goblinWizard": preload("res://Scenes/Enemies/Wizard.tscn")
+enum enemyTypes {
+	RANDOM,
+	GOBLIN,
+	GOBLIN_WIZARD,
 }
 
-func summonGobWizard() -> void:
-	var enemy = enemyScenes["goblinWizard"].instantiate()
-	enemy.global_position = global_position
-	get_parent().add_child(enemy) # set it to be child of the current room
+@export var enemies := {
+	enemyTypes.GOBLIN: "res://Scenes/Enemies/Goblin.tscn",
+	enemyTypes.GOBLIN_WIZARD: "res://Scenes/Enemies/Wizard.tscn"
+}
+
+var enemyToSpawn : enemyTypes = enemyTypes.RANDOM
+
+@export var spawnInterval := 2.0
+
+@onready var timer :Timer = $Timer
+
+var active := false
+
+func _ready() -> void:
+	timer.wait_time = spawnInterval
+	timer.timeout.connect(_on_timer_timeout)
 	
-func summonGob() -> void:
-	var enemy = enemyScenes["goblin"].instantiate()
-	enemy.global_position = global_position
-	get_parent().add_child(enemy)
+func activate() -> void:
+	if active:
+		return
+	active = true
+	timer.start()
 	
-func summonRandom() -> void: 
-	var k = enemyScenes.keys().pick_random()
-	var enemy = enemyScenes[k].instantiate()
+func _on_timer_timeout() -> void:
+	var path: String
+	if enemyToSpawn == enemyTypes.RANDOM:
+		path = enemies.values().pick_random()
+	else:
+		path = enemies[enemyToSpawn]
+	var scene = load(path)
+	spawnEnemy(scene)
+	
+func deactivate() -> void:
+	active = false
+	timer.stop()
+	
+func spawnEnemy(scene: PackedScene):
+	var enemy = scene.instantiate()
+	enemy.global_position = global_position
 	get_parent().add_child(enemy)
